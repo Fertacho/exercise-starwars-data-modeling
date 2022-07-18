782b805686ad725284d70dfe855a43116e67681f
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-07-18 19:56:39 +0000

    avance

--- src/models.py
+++ src/models.py
@@ -8,12 +8,41 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+class Users(Base):
+    __tablename__='users'
+    id = Column(Integer,primary_key=True)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250),nullable=False)
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
+class Login(Base):
+    __tablename__ = 'login'
+    id = Column(Integer, primary_key=True)
+    user_email = Column(String(250),ForeignKey('users.email'))
+    user_pass = Column(String(250),ForeignKey('users.password'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)
+    
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250),nullable=False)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250),nullable=False)
+
 
 class Address(Base):
     __tablename__ = 'address'
